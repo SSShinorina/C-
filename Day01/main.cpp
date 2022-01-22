046ce5c6da2f52b7c4b603154bf08b4a230bc1f1
shaon <shaon20aust@gmail.com>
2022-01-22 12:37:15 +0100

    C++ from beginning

---
+++ Day01/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Shaon" << endl;
+
+    cout <<"    /|"<< endl;
+    cout <<"   / |"<< endl;
+    cout <<"  /  |"<< endl;
+    cout <<" /   |"<< endl;
+    cout <<"/____|"<< endl;
+
+    string characterName = "Shaon";
+    int characterAge;
+    characterAge = 25;
+    cout <<"Hi!! This is " << characterName <<endl;
+    cout <<"And her age is " << characterAge << "." <<endl;
+
+    characterName = "Ishabela";
+    characterAge = 56;
+    cout <<"This is "<<characterName<<"who has 2 daughters."<< endl;
+    cout <<"Her age is "<< characterAge <<"." << endl;
+
+    char grade = 'A' ;
+    int age = 25;
+    bool isFemale = true;
+    string name = "Shaon";
+    double a1= 19.00;
+    float a2 = 19.0;
+
+    return 0;
+}
