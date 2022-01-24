56e75d8e202cd6337f87a7f6ea5ddd9ce8d346b2
shaon <shaon20aust@gmail.com>
2022-01-24 23:53:13 +0100

    Day 2 and 3 of C++

---
+++ Day_2/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void testFunc( string name, int age){
+    cout << "This is "<<  name << " and age is " << age << " years old." <<endl;
+}
+
+double cube(double number){
+    double result = number * number * number;
+    return result;
+}
+
+int main()
+{
+    string phrase = "Ihaon";
+    phrase[0] = 'S';
+    cout << phrase <<endl;
+
+    string phrase1 = "Shinorina Shahrin Shaon";
+    cout << phrase1.find("Shaon") << endl;
+    string phrasesub = phrase1.substr(4,2);
+    cout << phrasesub << endl;
+
+    cout << 5*(7+10) << endl;
+
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "You are " << age << " years old ." << endl;
+
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello " << name <<endl;
+
+    double num1, num2;
+    cout << "Enter 1st number: ";
+    cin >> num1;
+
+    cout << "Enter 2nd number: ";
+    cin >> num2;
+
+    cout << num1 + num2 << endl;
+    int numbers[20] = {1,2,3,4,5,6};
+    numbers[10] = 100;
+    cout << numbers[20] <<endl;
+
+    testFunc("Shaon", 25);
+    testFunc("Ishabela", 55);
+    testFunc("Sattar", 65);
+
+    cout << cube(5) <<endl;
+
+    bool isMale = false;
+    bool isMuslim = true;
+
+    if (isMale && isMuslim){
+        cout << "You are male and muslim";
+    }
+    else if(!isMale && isMuslim){
+        cout << "You are female and muslim";
+    }
+    else if(isMale && ! isMuslim){
+        cout << "You are male but not muslim";
+    }
+    else{
+        cout << "You are female but not muslim";
+    }
+
+    return 0;
+}
