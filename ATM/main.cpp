223b8c56f8dd3794a2d52755f75e67b299f47fec
shaon <shaon20aust@gmail.com>
2022-03-09 22:36:18 +0100

    ATM project work with C++

---
+++ ATM/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+void showMenu(){
+    cout<< "*****Menu*****" <<endl;
+    cout<< "1. Check Balance" <<endl;
+    cout<< "2. Deposit" <<endl;
+    cout<< "3. Withdraw" <<endl;
+    cout<< "4. Exit" <<endl;
+    cout<< "**********" <<endl;
+}
+
+int main()
+{
+    int option;
+    double balance = 500;
+    do{
+        showMenu();
+    cout << "Option: ";
+    cin >> option;
+    system("cls");
+
+    switch(option){
+        case 1: cout << "Balance is: "<< balance << "$" << endl;
+        break;
+        case 2: cout << "Deposit amount is: ";
+        double depositAmount;
+        cin >> depositAmount;
+        balance+= depositAmount;
+        break;
+        case 3: cout << "Withdraw amount: ";
+        double withdrawAmount;
+        cin >> withdrawAmount;
+        if(withdrawAmount<=balance)
+            balance -= withdrawAmount;
+        else
+            cout <<"Out of amount!!!"<<endl;
+        break;
+    }
+    }while(option!=4);
+
+    system("pause>0");
+
+}
