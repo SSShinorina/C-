49761c226497028b8e46bc8b764b05fec77cefe6
shaon <shaon20aust@gmail.com>
2022-04-23 15:20:40 +0200

    C++ problem solve.

--- Day_2/main.cpp
+++ Day_2/main.cpp
@@ -87,6 +87,54 @@ int main()
             cout << endl;
     }
 
+    int fifty = 50;
+    int one_hundrad = 100;
+    int total = fifty+one_hundrad;
+    cout << "This is total: "<< total << endl;
+
+    float totalSalesPercentage = 0.58;
+    float salesThisYear = 8.68;
+    cout << endl << endl;
+    cout << "Total sales this year is ";
+    cout << totalSalesPercentage* salesThisYear << endl;
+
+    float stateTax = 0.04;
+    float countryTax = 0.02;
+    int purchase = 95;
+    float total_state_purchase = purchase * stateTax;
+    float total_country_purchase = purchase * countryTax;
+
+    cout << "State purchase amount is " << total_state_purchase << endl;
+    cout << "Country purchase amount is " << total_country_purchase << endl;
+
+    float Tax = 0.0675;
+    float Tip = 0.20;
+
+    float meal_charge = 88.67;
+    float meal_tax_charge = meal_charge * Tax;
+    float meal_tip_charge = meal_charge * Tip;
+    float total_bill = meal_charge + meal_tax_charge + meal_tip_charge;
+
+    cout << "Total cost "<<meal_charge << endl;
+    cout << "Tax " << meal_tax_charge << endl;
+    cout << "Tip " << meal_tip_charge << endl;
+    cout << "Total " << total_bill << endl;
+
+
+    int a = 28;
+    int b = 32;
+    int c = 37;
+    int d = 24;
+    int e = 33;
+    int sum = a+b+c+d+e;
+    int avr = sum/5;
+    cout << "The average is: " << avr << endl;
+
+    double periods = 26;
+    double pay_amount = 2200;
+    double annual_pay = pay_amount * periods;
+    cout << "The annual pay is: " << annual_pay << endl;
+
 
     return 0;
 }
